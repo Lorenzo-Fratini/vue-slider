dfeddd00b9614e5ea0c8d82f4646a456aeed8b57
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-04-07 14:48:05 +0200

    initial update

---
+++ js/script.js
@@ -0,0 +1,65 @@
+function init() {
+  
+  new Vue({
+
+    el: '#app',
+
+    data: {
+
+      'defaultImg' : 'earth.png',
+      'image' : 'earth.png',
+      'image2' : 'earth2.png',
+      'image3' : 'earth-night.png'
+      
+    },
+
+    methods: {
+
+      nextImg: function() {
+
+        if (this.defaultImg == this.image) {
+
+          this.defaultImg = this.image2;
+        } else if (this.defaultImg == this.image2) {
+
+          this.defaultImg = this.image3;
+        } else if (this.defaultImg == this.image3) {
+
+          this.defaultImg = this.image;
+        }
+
+      },
+
+      prevImg: function() {
+
+        if (this.defaultImg == this.image) {
+
+          this.defaultImg = this.image3;
+        } else if (this.defaultImg == this.image2) {
+
+          this.defaultImg = this.image;
+        } else if (this.defaultImg == this.image3) {
+
+          this.defaultImg = this.image2;
+        }
+
+      },
+
+      timeNext: function() {
+
+        setInterval(() => { this.nextImg();}, 3000);
+      }
+    },
+
+    mounted () {
+
+      this.timeNext()
+
+    }
+  });
+
+   
+
+}
+
+$(init);
